8960cf27f2bb8c271177e7027e6e794febe76b60
Shayon G <gshayon122@hotmail.com>
2021-09-21 18:51:16 +0530

    basic routing added

--- index.js
+++ index.js
@@ -12,7 +12,21 @@ app.get('/', (req, res) => {
     res.render('home');
 })
 
+app.get('/New', (req, res) => {
+    res.render('New');
+})
 
+app.get('/Quiz', (req, res) => {
+    res.render('Quiz');
+})
+
+app.get('/Contact', (req, res) => {
+    res.render('Contact');
+})
+
+app.get('/About', (req, res) => {
+    res.render('About');
+})
 
 app.listen(3000, () =>{
     console.log("listening on port 3000");
